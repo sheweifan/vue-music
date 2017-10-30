76eb60834fba5ed24cf4eb603cee96b66eb1efd7
weifan she <923986234@qq.com>
2017-10-30 13:53:36 +0800

    backup

--- src/container/detail/index.vue
+++ src/container/detail/index.vue
@@ -1,36 +1,39 @@
 <template>
-	<div>
-		<appHeader title="详情" goback="true"></appHeader>
-		<div class="loading" v-if="loading">正在加载中...</div>
-		<div v-else>
-			<div class="topimg">
-				<img :src="data.pic" alt="">
-			</div>
-			<div class="infobox">
-				<p class="title">{{data.title}}</p>
-				<p class="total">
-					<span class="saleDesc">{{data.saleDesc}}</span>
-					<span class="price">{{data.price | money}}</span>
-					<del class="originalPrice">{{data.originalPrice | money}}</del>
-				</p>
-				<p class="content">
-					{{data.content}}
-				</p>
-				<p class="tags">
-					<span v-for="item in data.extList[0].itemList">{{item.title}}</span>
-				</p>
-			</div>
-			<div class="likebox">
-				<p class="title">{{data.extList[1].head}}</p>
-				<div class="likebox_list">
-					<div class="likebox_item" v-for="item in data.extList[1].itemList">
-						<img :src="item.pic" alt="">
-						<span class="desc">{{item.desc}}</span>
+
+  <transition name="slide-fade">
+		<div class="detail_container">
+			<appHeader title="详情" goback="true"></appHeader>
+			<div class="loading" v-if="loading">正在加载中...</div>
+			<div v-else>
+				<div class="topimg">
+					<img :src="data.pic" alt="">
+				</div>
+				<div class="infobox">
+					<p class="title">{{data.title}}</p>
+					<p class="total">
+						<span class="saleDesc">{{data.saleDesc}}</span>
+						<span class="price">{{data.price | money}}</span>
+						<del class="originalPrice">{{data.originalPrice | money}}</del>
+					</p>
+					<p class="content">
+						{{data.content}}
+					</p>
+					<p class="tags">
+						<span v-for="item in data.extList[0].itemList">{{item.title}}</span>
+					</p>
+				</div>
+				<div class="likebox">
+					<p class="title">{{data.extList[1].head}}</p>
+					<div class="likebox_list">
+						<div class="likebox_item" v-for="item in data.extList[1].itemList" @click="runToDetail(item.itemId)">
+							<img :src="item.pic" alt="">
+							<span class="desc">{{item.desc}}</span>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+  </transition>
 </template>
 
 <script>
@@ -55,6 +58,17 @@
 					this.loading = false
 					this.data = data.data
 				})
+		},
+		methods: {
+			runToDetail: function(_id){
+				fetch(detail + '?id=' + _id)
+					.then(response => response.json())
+					.then(data => {
+						console.log(data)
+						this.loading = false
+						this.data = data.data
+					})
+			}
 		}
 	}
 </script>
@@ -140,5 +154,19 @@
 		text-align: right;
 		color: #fff;
 		text-shadow: 0 0 2px rgba(0,0,0,0.5)
+	}
+
+	/* 可以设置不同的进入和离开动画 */
+	/* 设置持续时间和动画函数 */
+	.slide-fade-enter-active {
+	  transition: all .3s;
+	}
+	.slide-fade-leave-active {
+	  /*transition: all 0*/
+	}
+	.slide-fade-enter, .slide-fade-leave-to
+	/* .slide-fade-leave-active for below version 2.1.8 */ {
+	  /*transform: translateX(10px);*/
+	  opacity: 0;
 	}
 </style>
