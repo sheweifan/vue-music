24ad4ddcd7fb16a25e13ebb790c48c57e488c915
weifan she <923986234@qq.com>
2017-11-01 17:59:30 +0800

    add api

---
+++ src/api/index.js
@@ -0,0 +1,102 @@
+import axios from 'axios'
+
+// recommend
+export function getRecommend(){
+  const url = '/api/getRecommend'
+  return axios.get(url).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+export function getDiscList(){
+  const url = '/api/getDiscList'
+  return axios.get(url).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+export function getSongList(disstid){
+  const url = '/api/getDiscList'
+  return axios.get(url, {
+    params: {
+      disstid
+    }
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+// rank
+export function getTopList(){
+  const url = '/api/getTopList'
+  return axios.get(url).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+export function getMusicList(topid){
+  const url = '/api/getMusicList'
+  return axios.get(url, {
+    params: {
+      topid
+    }
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+// search
+export function getHotKey(){
+  const url = '/api/getHotKey'
+  return axios.get(url, {
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+export function search(query, page, zhida, perpage){
+  const url = '/api/search'
+  return axios.get(url, {
+    params: {
+      w: query,
+      p: page,
+      perpage,
+      n: perpage,
+      catZhida: zhida ? 1 : 0
+    }
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+// singer
+export function getSingerList(){
+  const url = '/api/getSingerList'
+  return axios.get(url, {
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+export function getSingerDetail(singermid){
+  const url = '/api/getSingerList'
+  return axios.get(url, {
+    params: {
+      singermid
+    }
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+// singer
+export function getLyric(songmid){
+  const url = '/api/getSingerList'
+  return axios.get(url, {
+    params: {
+      songmid
+    }
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
