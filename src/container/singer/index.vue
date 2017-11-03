33cea79b4f9ae24080a723631e125f8ceff3b3e5
weifan she <923986234@qq.com>
2017-11-03 11:17:33 +0800

    add link

--- src/container/singer/index.vue
+++ src/container/singer/index.vue
@@ -1,43 +1,64 @@
 <template lang="pug">
 div
-  mt-index-list
-    mt-index-section.singer-list(index="A")
-      router-link.singer-list-item(tag="div" to="/")
-        div.img
-          img(src="https://y.gtimg.cn/music/photo_new/T001R300x300M0000025NhlN2yWrP4.jpg?max_age=2592000")
-        p.title 周杰伦
-      router-link.singer-list-item(tag="div" to="/")
-        div.img
-          img(src="https://y.gtimg.cn/music/photo_new/T001R300x300M0000025NhlN2yWrP4.jpg?max_age=2592000")
-        p.title 周杰伦
-      router-link.singer-list-item(tag="div" to="/")
-        div.img
-          img(src="https://y.gtimg.cn/music/photo_new/T001R300x300M0000025NhlN2yWrP4.jpg?max_age=2592000")
-        p.title 周杰伦
-      router-link.singer-list-item(tag="div" to="/")
-        div.img
-          img(src="https://y.gtimg.cn/music/photo_new/T001R300x300M0000025NhlN2yWrP4.jpg?max_age=2592000")
-        p.title 周杰伦
-      router-link.singer-list-item(tag="div" to="/")
-        div.img
-          img(src="https://y.gtimg.cn/music/photo_new/T001R300x300M0000025NhlN2yWrP4.jpg?max_age=2592000")
-        p.title 周杰伦
+  div.singer-container#singer-container
+    mt-index-list
+      mt-index-section.singer-list(v-for="(item, index) of list" :index="item[0].title" :key="item.index")
+        router-link.singer-list-item(v-for="(child, index) of item" tag="div" :to="'/singer/'+child.id" :key="item.id")
+          div.img
+            img(v-lazy="child.imgUrl")
+          p.title {{child.name}}
+  router-view
 </template>
 
 <script>
+  import { map, groupBy, filter, values, sortBy, take } from 'lodash'
+  import { getSingerList } from '@/api'
+
+  const singerDataFormat = (data) => {
+    let list = filter(map(data, (item, index) => {
+      return {
+        id: item.Fsinger_mid,
+        title: item.Findex,
+        name: item.Fsinger_name,
+        imgUrl: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+      }
+    }), (item) => !!item.title.match(/[a-zA-Z]/))
+
+    let listGroup = groupBy(list, 'title')
+    listGroup = values(listGroup)
+    listGroup = sortBy(listGroup, (item) => item[0].title)
+    const hot = map(take(list, 10), (item) => ({
+      ...item,
+      title: 'hot'
+    }))
+    listGroup = [hot].concat(listGroup)
+    return listGroup
+  }
+
   export default {
     name: 'singer',
     data: function(){
       return {
-        list: new Array(10).fill(56)
+        list: {}
       }
+    },
+    methods: {
+      getSingerList: async function(){
+        const { data, success } = await getSingerList()
+        if (success){
+          const { list } = data
+          this.list = singerDataFormat(list)
+        }
+      }
+    },
+    created: function(){
+      this.getSingerList()
     }
   }
 </script>
 
 <style lang="stylus" scoped>
   @import '../../static/stylus/index.styl'
-
   .singer-list-item
     padding: $spacing
     display: flex
