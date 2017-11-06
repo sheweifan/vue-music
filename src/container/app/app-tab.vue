d4e86919d9998c01ddf21f07c9556042bfcda663
weifan she <923986234@qq.com>
2017-11-06 17:48:49 +0800

    mkdir for app components

---
+++ src/container/app/app-tab.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+    div.app-tab
+      span.left
+      tabbar(:data="tab")
+      span.right
+</template>
+
+<script>
+  import tabbar from '@/components/tabbar.vue'
+  export default {
+    name: 'app',
+    components: {
+      tabbar
+    },
+    data: function(){
+      return {
+        tab: [
+          {
+            name: '推荐',
+            link: '/recommend'
+          },
+          {
+            name: '歌手',
+            link: '/singer'
+          },
+          {
+            name: '排行',
+            link: '/rank'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .app-tab
+    position: fixed
+    z-index: 1
+    left: 0
+    width: 100%
+    top: 0
+    display: flex
+    background-color: #fff
+    .tabbar
+      flex: 1
+    .left
+      width: 50px
+    .right
+      width: 50px
+</style>
