ca64e500a284b3b510a87d19b2f9e70b388e5188
weifan she <923986234@qq.com>
2017-11-06 17:46:29 +0800

    add vuex

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,21 @@
 <template lang="pug">
   div#app
     div.app-tab-brank
-    div.app-tab
-      span.left
-      tabbar(:data="tab")
-      span.right
+    app-tab
     keep-alive
       router-view
+    player
 </template>
 
 <script>
 	import '../static/css/reset.css'
-  import tabbar from '@/components/tabbar.vue'
+  import appTab from '@/container/app/app-tab.vue'
+  import player from '@/container/player'
 	export default {
 		name: 'app',
     components: {
-      tabbar
-    },
-    data: function(){
-      return {
-        tab: [
-          {
-            name: '推荐',
-            link: '/recommend'
-          },
-          {
-            name: '歌手',
-            link: '/singer'
-          },
-          {
-            name: '排行',
-            link: '/rank'
-          }
-        ]
-      }
+      appTab,
+      player
     }
 	}
 </script>
@@ -41,18 +23,4 @@
 <style lang="stylus" scoped>
   .app-tab-brank
     padding-top: 52px
-  .app-tab
-    position: fixed
-    z-index: 1
-    left: 0
-    width: 100%
-    top: 0
-    display: flex
-    background-color: #fff
-    .tabbar
-      flex: 1
-    .left
-      width: 50px
-    .right
-      width: 50px
 </style>
