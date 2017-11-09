250e597d2b16f98f0a5f9c8b1007b9aa188d3c76
weifan she <923986234@qq.com>
2017-11-09 09:31:27 +0800

    add format

--- src/utils/base642Lyric.js
+++ src/utils/base642Lyric.js
@@ -1,8 +1,16 @@
 import Lyric from 'lyric-parser'
-import {Base64} from 'js-base64'
+import { Base64 } from 'js-base64'
+import { map } from 'lodash'
 
 export const base642Lyric = (str) => {
   const parse = Base64.decode(str)
   let lyric = new Lyric(parse)
+  // lyric = map(lyric, )
+  lyric.lines = map(lyric.lines, (item) => {
+    return {
+      ...item,
+      time: item.time / 1000
+    }
+  })
   return lyric
 }
