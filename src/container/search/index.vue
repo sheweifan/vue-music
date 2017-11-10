e9173c2c49af14a5c2f4d633f60f38335d7f7828
weifan she <923986234@qq.com>
2017-11-10 17:44:03 +0800

    add container search

---
+++ src/container/search/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  transition(name="slide")
+    div.search-container
+      searchBar(
+        placeholder="搜索歌曲、歌手"
+        @input="searchInput"
+        @cancel-click="cancelClick"
+      )
+</template>
+
+<script>
+  import vHead from '@/components/v-head.vue'
+  import scrollView from '@/components/scroll-view.vue'
+  import searchBar from '@/components/search-bar.vue'
+  export default {
+    name: 'search',
+    components: {
+      vHead,
+      scrollView,
+      searchBar
+    },
+    data(){
+      return {
+      }
+    },
+    methods: {
+      searchInput(val){
+        console.log(val)
+      },
+      cancelClick(){
+        this.$router.go(-1)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../static/stylus/index.styl'
+  .search-container
+    fixed-page()
+    
+</style>
