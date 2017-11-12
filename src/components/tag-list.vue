b1d0394e30730f5f422d561f9e678f7dbdd69a5d
dolor <923986234@qq.com>
2017-11-12 17:12:55 +0800

    add component tag-list

---
+++ src/components/tag-list.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  div.tag-list
+    span.tag-list-item(v-for="(item, index) in data" :key="index") {{ item.text }}
+</template>
+
+<script>
+  export default {
+    name: 'tag-list',
+    props: ['data'],
+    methods: {
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../static/stylus/index.styl'
+
+  .tag-list
+    padding: $spacing 0 0 $spacing
+    .tag-list-item
+      display: inline-block
+      font-size: $fontSize
+      line-height: 1.25
+      padding: 2px $spacing
+      margin-right: $spacing
+      margin-bottom: $spacing
+      background: rgba(0,0,0,0.1)
+      border-radius: $radius
+      color: #888
+</style>
