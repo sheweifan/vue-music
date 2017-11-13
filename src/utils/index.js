4e4bfe58353261951b8db94bc7d503daf5ce3c53
weifan she <923986234@qq.com>
2017-11-13 16:07:31 +0800

    add song, singer

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,11 @@
 import { base642Lyric } from './base642Lyric'
+import * as storage from './storage'
+import * as song from './song'
+import * as singer from './singer'
 
 export {
-  base642Lyric
+  base642Lyric,
+  storage,
+  song,
+  singer
 }
