f900f19d1c6297e8c201659958f01559bf991485
weifan she <923986234@qq.com>
2017-11-13 16:07:08 +0800

    backup

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,9 +1,9 @@
 const debug = process.env.NODE_ENV !== 'production'
 
-export const getItem = (key) => {
+export const get = (key) => {
   let value
   try {
-    value = window.localStorage.getItem(key)
+    value = JSON.parse(window.localStorage.getItem(key))
   } catch (e) {
     if (debug) {
       throw new Error('浏览器不支持localStorage!')
@@ -12,9 +12,9 @@ export const getItem = (key) => {
   return value
 }
 
-export const setItem = (key, value) => {
+export const set = (key, value) => {
   try {
-    window.localStorage.setItem(key, value)
+    window.localStorage.setItem(key, JSON.stringify(value))
   } catch (e) {
     if (debug) {
       throw new Error('浏览器不支持localStorage!')
@@ -22,7 +22,7 @@ export const setItem = (key, value) => {
   }
 }
 
-export const removeItem = (key) => {
+export const remove = (key) => {
   try {
     window.localStorage.removeItem(key)
   } catch (e) {
