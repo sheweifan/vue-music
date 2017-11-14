7de8037b3a30afdc7a578e1071e2a8e610455073
weifan she <923986234@qq.com>
2017-11-14 11:10:16 +0800

    add component control-item list-view

---
+++ src/components/control-item.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  div.control-item(@click="itemClick")
+    icon.control-item-icon(:name="icon")
+    span.control-item_title
+      slot
+</template>
+
+<script>
+  import icon from './icon.vue'
+  
+  export default {
+    name: 'tag-list',
+    components: {
+      icon
+    },
+    props: ['data', 'icon', 'index'],
+    methods: {
+      itemClick(){
+        this.$emit('click')
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../static/stylus/index.styl'
+  .control-item
+    font-size: $fontSize
+    padding: $spacing
+    color: #666
+    position: relative
+    display: flex
+    justify-content: center
+    align-items: center
+    &:not(:last-child):before
+      sethalfborderbottom()
+      left: $spacing
+      right: $spacing
+  .control-item-icon
+    width: 20px
+    height: 20px
+    margin-right: $spacing
+  .control-item_title
+    flex: 1
+</style>
---
+++ src/components/list-view.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+  scroll-view(watchs="data" @pullUp="pullUp")
+    slot
+    div.list-view-footer
+      mt-spinner(:type="2" v-if="loading")
+      p(v-else-if="pageIndex === totalPage") 没有更多了
+      p(v-else="pageIndex < totalPage") 上拉加载更多
+</template>
+
+<script>
+  import scrollView from './scroll-view.vue'
+  
+  export default {
+    name: 'list-view',
+    props: [
+      {
+        data: {
+          default: [],
+          type: Array
+        },
+        pageTotal: {
+          default: 1,
+          type: Number
+        }
+      },
+      'totalPage'
+    ],
+    data(){
+      return {
+        loading: false,
+        pageIndex: 0
+      }
+    },
+    components: {
+      scrollView
+    },
+    methods: {
+      _endLoad(){
+        this.loading = false
+      },
+      _load(){
+        this.loading = true
+        this.pageIndex++
+        this.$emit('loadMore', this.pageIndex, this._endLoad)
+      },
+      pullUp(){
+        if (this.loading || this.pageIndex === this.totalPage) return
+        this._load()
+      }
+    },
+    created(){
+      this.$nextTick(this._load)
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../static/stylus/index.styl'
+  .list-view-footer
+    display: flex
+    justify-content: center
+    align-items: center
+    height: 42px
+    font-size: $fontSize
+    color: #999
+</style>
