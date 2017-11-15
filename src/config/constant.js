0e27fad0dab7577eb467644fe2ef87895ccffa12
weifan she <923986234@qq.com>
2017-11-15 16:20:29 +0800

    add storage keys

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,3 +1,5 @@
 export default {
-  searchHistoryStorageKey: '__search__history__'
+  searchHistoryStorageKey: '__search__history__',
+  collectHistoryStorageKey: '__collect__history__',
+  playHistoryStorageKey: '__play__history__'
 }
