ad82142d81bc13291930cf17cff063d39ed2d222
weifan she <923986234@qq.com>
2017-11-16 09:12:15 +0800

    add component loading

---
+++ src/components/loading.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div.loading(:style="{ height: height+'px' }")
+    mt-spinner(:type="2")
+</template>
+
+<script>
+  export default {
+    name: 'loading',
+    props: ['height']
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../static/stylus/index.styl'
+  .loading
+    display: flex
+    justify-content: center
+    align-items: center
+    min-height: 42px
+    height: 100%
+    font-size: $fontSize
+    color: #999
+</style>
