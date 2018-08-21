80fc2419262138ef45a6a2f106615cfcf67fa95f
dolor <923986234@qq.com>
2018-08-21 23:06:11 +0800

    add

--- src/components/information.vue
+++ src/components/information.vue
@@ -86,107 +86,87 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-@keyframes moveUpMsg {
-	0% {
-    opacity: 0;
-    transform: translate(-50%, 30%);
-	}
-	100% {
-		opacity: 1;
-	  transform: translate(-50%, 0);
-	}
-}
+<style lang="stylus" scoped>
+@keyframes moveUpMsg
+	0%
+    opacity: 0
+    transform: translate(-50%, 30%)
+	100%
+		opacity: 1
+	  transform: translate(-50%, 0)
 
-@keyframes moveDownMsg {
-	0% {
-		opacity: 1;
-	  transform: translate(-50%, 0);
-	}
-	100% {
-		opacity: 0;
-	  transform: translate(-50%, 30%);
-	}
-}
+@keyframes moveDownMsg
+	0%
+		opacity: 1
+	  transform: translate(-50%, 0)
+	100%
+		opacity: 0
+	  transform: translate(-50%, 30%)
 
-.msg-modal-box {
-	// display: none;
-	.modal-mask {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background-color: rgba(0,0,0,.3);
-		z-index: 10;
-	}
-	.modal-cont {
-		position: fixed;
-		bottom: 15px;
-		left: 50%;
-		transform: translate(-50%, 30%);
-		width: 345px;
-		height: 200px;
-		background-color: #fff;
-		border-radius: 10px;
-		z-index: 11;
-		&.move-up {
-			animation: moveUpMsg .3s ease-out forwards;
-		}
-		&.move-down {
-			animation: moveDownMsg .3s ease-out forwards;
-		}
-		.msg-header {
-			position: relative;
-			height: 48px;
-			border-bottom: 1px solid #eaeaea;
-			.title {
-        font-size: 16px;
+.msg-modal-box
+	// display: none
+	.modal-mask
+		position: fixed
+		top: 0
+		left: 0
+		width: 100%
+		height: 100%
+		background-color: rgba(0,0,0,.3)
+		z-index: 10
+	.modal-cont
+		position: fixed
+		bottom: 15px
+		left: 50%
+		transform: translate(-50%, 30%)
+		width: 345px
+		height: 200px
+		background-color: #fff
+		border-radius: 10px
+		z-index: 11
+		&.move-up
+			animation: moveUpMsg .3s ease-out forwards
+		&.move-down
+			animation: moveDownMsg .3s ease-out forwards
+		.msg-header
+			position: relative
+			height: 48px
+			border-bottom: 1px solid #eaeaea
+			.title
+        font-size: 16px
 				line-height: 48px
-				color: #333;
-				text-align: center;
-			}
-			.cancel-btn {
-				position: absolute;
-				top: 0;
-				left: 0;
-				padding: 0 pxToRem(30);
-				height: pxToRem(97);
-				line-height: pxToRem(97);
-				@include font-dpr(15px);
-				color: #999;
-			}
-			.send-btn {
-				position: absolute;
-				top: 0;
-				right: 0;
-				padding: 0 15px;
-				height: 48px;
-				line-height: 48px;
-        font-size: 15px;
-				color: #ff745b;
-			}
-		}
-		.msg-wrap {
-			padding: 12px;
-			.msg-txt {
-				width: 100%;
-				height: 110px;
-				resize: none;
-        font-size: 14px;
-				color: #333;
-        line-height: 22px;
-
-        &::-webkit-input-placeholder{
-          color: #ccc;
-        }
-			}
-			.letter-limit {
-        font-size: 12px;
-				color: #ccc;
-				text-align: right;
-			}
-		}
-	}
-}
+				color: #333
+				text-align: center
+			.cancel-btn
+				position: absolute
+				top: 0
+				left: 0
+				padding: 0 pxToRem(30)
+				height: pxToRem(97)
+				line-height: pxToRem(97)
+				@include font-dpr(15px)
+				color: #999
+			.send-btn
+				position: absolute
+				top: 0
+				right: 0
+				padding: 0 15px
+				height: 48px
+				line-height: 48px
+        font-size: 15px
+				color: #ff745b
+		.msg-wrap
+			padding: 12px
+			.msg-txt
+				width: 100%
+				height: 110px
+				resize: none
+        font-size: 14px
+				color: #333
+        line-height: 22px
+        &::-webkit-input-placeholder
+          color: #ccc
+			.letter-limit
+        font-size: 12px
+				color: #ccc
+				text-align: right
 </style>
