64a86a346fce9344f8960e325edb531514c74822
dolor <923986234@qq.com>
2018-08-24 22:51:58 +0800

    add

--- src/components/scroll-view.vue
+++ src/components/scroll-view.vue
@@ -60,7 +60,7 @@
 
 <style lang="stylus" scoped>
   @import '../static/stylus/index.styl'
-    
+
   .scroll-wrapper
     background: #fff
     position: absolute
@@ -75,20 +75,8 @@
     width: 100%
     min-height: 100%
     -webkit-tap-highlight-color: rgba(0,0,0,0)
-    -webkit-transform: translateZ(0)
-    -moz-transform: translateZ(0)
-    -ms-transform: translateZ(0)
-    -o-transform: translateZ(0)
     transform: translateZ(0)
-    -webkit-touch-callout: none
-    -webkit-user-select: none
-    -moz-user-select: none
-    -ms-user-select: none
     user-select: none
-    -webkit-text-size-adjust: none
-    -moz-text-size-adjust: none
-    -ms-text-size-adjust: none
-    -o-text-size-adjust: none
     text-size-adjust: none
 
 </style>
