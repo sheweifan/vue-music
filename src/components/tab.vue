ae494292f6a7d61afd2ccadd54f334260c0084fb
dolor <923986234@qq.com>
2018-08-30 21:41:14 +0800

    add

--- src/components/tab.vue
+++ src/components/tab.vue
@@ -62,59 +62,45 @@ export default {
 }
 </script>
 
-<style scope lang="scss">
-  @import "../assets/sass/config";
-  @import "../assets/sass/function";
-  @import "../assets/sass/mixin";
-  .tab-list{
-    &.fixed{
-      height: 45px;
-      .tab-list-container{
-        position: fixed;
-        z-index: 10;
-        width: pxToRem(750);
-        left: 50%;
-        transform: translateX(-50%);
-      }
-    }
-  }
-  .tab-list-container{
-    display: flex;
-    width: 100%;
-    background-color: #F3F4F5;
-    border-bottom: 1px solid #ddd;
-  }
-  .tab-list-item{
-    flex: 1;
-    text-align: center;
-
-    &.active {
-      .tab-list-item-default{
-      color: #459cf3;
-      border-bottom: 1px solid #459cf3;
-      }
-    }
-
-    .bullet{
-      font-size: 11px;
-      color: #FFFFFF;
-      line-height: 15px;
-      background: #FF5B5B;
-      border-radius: 7.5px;
-      width: 20px;
-      height: 15px;
-      top: 5px;
-      position: absolute;
-      left: 100%;
-    }
-  }
-  .tab-list-item-default{
-    padding: 9px 0;
-    display: inline-block;
-    height: 25px;
-    line-height: 25px;
-    font-size: 15px;
-    color: #000;
-    position: relative;
-  }
+<style scope lang="stylus">
+  .tab-list
+    &.fixed
+      height: 45px
+      .tab-list-container
+        position: fixed
+        z-index: 10
+        width: pxToRem(750)
+        left: 50%
+        transform: translateX(-50%)
+  .tab-list-container
+    display: flex
+    width: 100%
+    background-color: #F3F4F5
+    border-bottom: 1px solid #ddd
+  .tab-list-item
+    flex: 1
+    text-align: center
+    &.active
+      .tab-list-item-default
+      color: #459cf3
+      border-bottom: 1px solid #459cf3
+    .bullet
+      font-size: 11px
+      color: #FFFFFF
+      line-height: 15px
+      background: #FF5B5B
+      border-radius: 7.5px
+      width: 20px
+      height: 15px
+      top: 5px
+      position: absolute
+      left: 100%
+  .tab-list-item-default
+    padding: 9px 0
+    display: inline-block
+    height: 25px
+    line-height: 25px
+    font-size: 15px
+    color: #000
+    position: relative
 </style>
